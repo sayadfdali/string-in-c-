a78a5cdc9939b9ac8993db3dd0e13e89a93f5ae1
sayadfdali <sayadfdali@gmail.com>
2025-12-25 02:11:03 +0600

    practicing string in c++

---
+++ string_capacity.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hello World";
+    s.resize(15, 'x');
+    cout << s << endl;
+    // cout << s.size() << endl;
+    return 0;
+}
