6717ca5b4d2ba50ed2cc863d0f9818ed33bdb38f
Sayad Fahad <sayadfdali@gmail.com>
2025-12-29 14:30:26 +0600

    Add files via upload

---
+++ U_New_Words.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int cnt[1000000] = {0};
+    
+    for(char c: s)
+    {
+        cnt[]
+    }
+    return 0;
+}
